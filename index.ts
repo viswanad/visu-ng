b5c949f09d0a1561148eb602f2849b5ed3880d56
Viswanada Yarram <viswanada.yarram@senecaglobal.net>
2018-05-25 14:57:41 +0530

    basic files

---
+++ index.ts
@@ -0,0 +1,4 @@
+
+export {MathService} from './src/app/math.service';
+export {SimpModModule} from './src/app/simp-mod/simp-mod.module';
+export {SimpComponent} from './src/app/simp/simp.component';
